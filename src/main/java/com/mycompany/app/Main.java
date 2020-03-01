48249c57e088ff69fb5ea3c1ada8b38dc34c7e47
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-03-01 21:10:01 +0900

    Add Dao

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -11,37 +11,41 @@ import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import com.mycompany.AppConfig;
+import org.seasar.doma.jdbc.Config;
 
 import java.util.concurrent.CompletionStage;
 
 public class Main extends AllDirectives {
 
-    public static void main(String[] args) throws Exception {
-        // boot up server using the route as defined below
-        ActorSystem system = ActorSystem.create("routes");
+  public static void main(String[] args) throws Exception {
+    // boot up server using the route as defined below
+    ActorSystem system = ActorSystem.create("routes");
 
-        final Http http = Http.get(system);
-        final ActorMaterializer materializer = ActorMaterializer.create(system);
+    Config config = AppConfig.singleton();
 
-        //In order to access all directives we need an instance where the routes are define.
-        Main app = new Main();
+    final Http http = Http.get(system);
+    final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
-        final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-          ConnectHttp.toHost("localhost", 8080), materializer);
+    //In order to access all directives we need an instance where the routes are define.
+    Main app = new Main();
 
-        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
-        System.in.read(); // let it run until user presses return
+    final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
+    final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
+      ConnectHttp.toHost("localhost", 8080), materializer);
 
-        binding
-          .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
-          .thenAccept(unbound -> system.terminate()); // and shutdown when done
-    }
+    System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+    System.in.read(); // let it run until user presses return
 
-    private Route createRoute() {
-        return concat(
-          path("hello", () ->
-            get(() ->
-              complete("<h1>Say hello to akka-http</h1>"))));
-    }
+    binding
+      .thenCompose(ServerBinding::unbind) // trigger unbinding from the port
+      .thenAccept(unbound -> system.terminate()); // and shutdown when done
+  }
+
+  private Route createRoute() {
+    return concat(
+      path("hello", () ->
+        get(() ->
+          complete("<h1>Say hello to akka-http</h1>"))));
+  }
 }
---
+++ src/main/java/com/mycompany/dao/EventDao.java
@@ -0,0 +1,8 @@
+package com.mycompany.dao;
+
+import com.mycompany.AppConfig;
+import org.seasar.doma.Dao;
+
+@Dao(config = AppConfig.class)
+public interface EventDao {
+}
