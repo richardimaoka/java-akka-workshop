33f119946c1a2cb0e12b80f4c9dd1a9388d5baa2
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-03-09 08:26:40 +0900

    Insert from OrderRoute

--- src/main/java/com/mycompany/route/OrderRoute.java
+++ src/main/java/com/mycompany/route/OrderRoute.java
@@ -2,22 +2,41 @@ package com.mycompany.route;
 
 import akka.http.javadsl.marshallers.jackson.*;
 import akka.http.javadsl.server.*;
+import com.mycompany.dao.*;
 import com.mycompany.dto.*;
+import com.mycompany.entity.*;
 import org.seasar.doma.jdbc.tx.*;
 
 public class OrderRoute extends AllDirectives {
   private TransactionManager transactionManager;
+  private OrderDao dao;
 
   public OrderRoute(TransactionManager transactionManager) {
     this.transactionManager = transactionManager;
+    this.dao = new OrderDaoImpl();
+  }
+
+  private Order convert(OrderPutRequest req) {
+    Order order = new Order();
+    order.setTicketId(req.getTicketId());
+    order.setUserId(req.getUserId());
+    order.setQuantity(1);
+    return order;
+  }
+
+  private void insert(Order order) {
+    transactionManager.required(() -> {
+      dao.insert(order);
+    });
   }
 
   public Route route () {
-   return pathPrefix("orders", () ->
+    return pathPrefix("orders", () ->
       pathEndOrSingleSlash(() ->
-        entity(Jackson.unmarshaller(OrderPutRequest.class), req ->
-          complete( "Put request received: ticket_id = " + req.getTicketId() + ", user_id = " + req.getUserId())
-        )
+        entity(Jackson.unmarshaller(OrderPutRequest.class), req -> {
+          insert(convert(req));
+          return complete( "Put request received: ticket_id = " + req.getTicketId() + ", user_id = " + req.getUserId() );
+        })
       )
     );
   }
