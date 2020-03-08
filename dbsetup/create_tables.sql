f9227b697618fd3697e2750d97b5c19443cec34f
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-03-08 17:12:03 +0900

    add initial data

--- dbsetup/create_tables.sql
+++ dbsetup/create_tables.sql
@@ -1,15 +1,15 @@
 -- USE java_akka_workshop;
 
 CREATE TABLE events (
-  `id` VARCHAR(10) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   -- phase
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE tickets (
-  `id` VARCHAR(10) NOT NULL,
-  `event_id` VARCHAR(10) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `event_id` INT NOT NULL,
   `name` VARCHAR(255) NOT NULL,
   `price` INT NOT NULL,
   `quantity` INT NOT NULL,
@@ -21,15 +21,15 @@ CREATE TABLE tickets (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE users (
-  `id` VARCHAR(10) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE orders (
-  `id` VARCHAR(10) NOT NULL,
-  `ticket_id` VARCHAR(10) NOT NULL,
-  `user_id` VARCHAR(10) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ticket_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   `quantity` INT NOT NULL,
   `created_at` DATETIME NOT NULL,
   PRIMARY KEY(`id`),
---
+++ dbsetup/initial_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO events (name) VALUES ("EXILE Tour 2020");
+INSERT INTO events (name) VALUES ("ゆず　ツアー 2020");
+INSERT INTO events (name) VALUES ("横浜ＤｅＮＡベイスターズ（公式戦）  ＤｅＮＡ　ＶＳ　広島 3/31(火)");
+
+
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (1, "2020/6/20(土)", 12800, 10, "2020-03-28 18:00:00", "2020-03-28 17:00:00");
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (1, "2020/6/27(土)", 12800, 10, "2020-03-28 18:00:00", "2020-03-28 17:00:00");
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (1, "2020/7/4(土)",  12800, 10, "2020-03-28 18:00:00", "2020-03-28 17:00:00");
+
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (2, "2020/3/28(土)", 9800, 10, "2020-03-28 17:00:00", "2020-03-28 16:00:00");
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (2, "2020/3/28(土)", 9800, 10,  "2020-03-29 17:00:00", "2020-03-28 16:00:00");
+
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (2, "2020/3/31(火) A席", 9800, 100, "2020-03-31 18:00:00", "2020-03-28 16:00:00");
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (2, "2020/3/31(火) B席", 7800, 200, "2020-03-31 18:00:00", "2020-03-28 16:00:00");
+INSERT INTO tickets (event_id, name, price, quantity, start_at, opening_at) VALUES (2, "2020/3/31(火) C席", 5800, 200, "2020-03-31 18:00:00", "2020-03-28 16:00:00");
